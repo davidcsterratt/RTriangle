56855b4fd17f3dd2c2522ffc35df2664a97fbf5e
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-03 14:53:37 +0000

    Initial version of triangle library and interface files added

---
+++ pkg/R/triangle.R
@@ -0,0 +1,28 @@
+if (is.loaded("R_triangulate")) dyn.unload("R_triangle.so")
+dyn.load("R_triangle.so")
+
+triangulate <- function(P, S=NULL, a=NULL, q=NULL, Y=FALSE) {
+  if (ncol(P) == 2) {
+    P <- t(P)
+  }
+  PB <- rep(1, ncol(P))
+  if (is.null(S)) {
+    S <- rbind(1:ncol(P), c(2:ncol(P),1))
+  } else {
+    if (ncol(S) == 2) {
+      S <- t(S)
+    }
+  }
+  SB <- rep(1, ncol(S))
+
+  out <- .Call("R_triangulate",
+               P,
+               as.integer(PB),
+               as.integer(S),
+               as.integer(SB),
+               a,
+               q,
+               Y)
+  names(out) <- c("P", "PB", "T", "S", "SB", "E", "EB")
+  return(out)
+}
