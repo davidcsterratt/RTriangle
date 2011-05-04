640e6910380d709a80e1b856eb0817fb9f701ae7
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-04 14:49:40 +0000

    Moved check.na.nan() into triangulate() First pass at documenting triangulate()
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/triangle@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/triangle.R
+++ pkg/R/triangle.R
@@ -1,3 +1,50 @@
+##' Generate exact Delaunay triangulations, constrained Delaunay
+##' triangulations, and high-quality triangular meshes using the
+##' Triangle library
+##' (\url{http://www.cs.cmu.edu/~quake/triangle.html})
+##'
+##' Triangulate a planar straight line graph comprising points
+##' \code{P} and, optionally, segments \code{S}. The
+##' triangulation is a constrained conforming Delaunay triangulation
+##' in which additional vertices, called Steiner points, can be
+##' inserted into segments to improved the quality of the
+##' triangulation.  To prevent the insertion of Steiner points on
+##' boundary segments, specify \code{Y=1}. If the maximum triangle area
+##' \code{a} is specified, the area of each triangle is not allowed to
+##' exceed this value. If the the minimum angle \code{q} is
+##' specified, no triangle angle is allowed to be below this value.
+##' @title Triangulate a Planar Straight Line Graph
+##' @param P Matrix of x-y co-ordinates of vertices, arranged either
+##' in rows or columns.
+##' @param S Matrix of segments, arranged either in rows or
+##' columns. Each segment refers to the indices in \code{P} of the
+##' endpoints of the segment.
+##' @param a Maximum triangle area. If specified, triangles cannot be
+##' larger than this area.
+##' @param q Minimum triangle angle in degrees.
+##' @param Y If \code{TRUE} prohibits the insertion of Steiner points
+##' on the mesh boundary.
+##' @param j If \code{TRUE} Jettisons vertices that are not part of
+##' the final triangulation from the output.
+##' @param V Verbosity level. Specify higher values  for more detailed
+##' information about what the Triangle library is doing.
+##' @param Q If \code{TRUE} suppresses all explanation of what the
+##' Triangle library is doing, unless an error occurs. 
+##' @return \item{P}{Set of vertices in the triangulation.}
+##' \item{PB}{Boundary markers of vertices. For each vertex this is 1
+##' if the point is on a boundary of the triangulation and zero
+##' otherwise.}
+##' \item{T}{Triangulation specified as 3 column matrix
+##' in which each row contains indices in \code{P} of vertices.}
+##' \item{S}{Set of segments (enforced edges) in the triangulation.}
+##' \item{SB}{Boundary markers of segments. For each segment this is 1
+##' if the point is on a boundary of the triangulation and 0
+##' otherwise.}
+##' \item{E}{Set of edges in the triangulation.}
+##' \item{EB}{Boundary markers of edges. For each edge this is 1 if
+##' the point is on a boundary of the triangulation and 0
+##' otherwise.}
+##' @author David Sterratt
 triangulate <- function(P, S=NULL, a=NULL, q=NULL, Y=FALSE, j=FALSE,
                         V=0, Q=FALSE) {
   ## It is necessary to check for NAs and NaNs, as the triangulate C
@@ -54,3 +101,5 @@ triangulate <- function(P, S=NULL, a=NULL, q=NULL, Y=FALSE, j=FALSE,
   names(out) <- c("P", "PB", "T", "S", "SB", "E", "EB")
   return(out)
 }
+
+# LocalWords:  param Sterratt
