61b7d4568af370538f95c44b04721e5f4b85d5fc
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-05 17:56:18 +0000

    * Changed names of arguments to triangulate() and output Voronoi Tesselation
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/triangle@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/basic.R
+++ pkg/tests/basic.R
@@ -1,5 +1,5 @@
 library(Triangle)
-P <- pslg(V=rbind(c(0, 0), c(0, 1), c(0.5, 0.5), c(1, 1), c(1, 0)),
+p <- pslg(P=rbind(c(0, 0), c(0, 1), c(0.5, 0.5), c(1, 1), c(1, 0)),
           S=rbind(c(1, 2), c(2, 3), c(3, 4), c(4, 5), c(5, 1)))
-TP <- triangulate(P)
-print(TP)
+tp <- triangulate(p)
+print(tp)
