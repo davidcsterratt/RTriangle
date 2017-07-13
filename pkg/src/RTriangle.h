2897b409b0a4cf3a2e737e91d7a4f2bc0a644bbf
davidcsterratt <david.c.sterratt@ed.ac.uk>
2017-07-13 15:06:18 +0100

    Fix for Issue #5 that works on Windows

--- pkg/src/RTriangle.h
+++ pkg/src/RTriangle.h
@@ -5,3 +5,9 @@
 #undef PI
 #define printf Rprintf
 #define TRILIBRARY
+/* Needed to ensure test test-triangulate/"triangulate can triangulate
+   an example that has crashed on Win i386" passes on Windows 32
+   bit. See triangle.c for explanation of WIN32. */
+#ifdef _WIN32
+#define CPU86
+#endif /* _WIN32 */
